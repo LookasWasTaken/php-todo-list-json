6447b015c35e26e14d4dbb5618e1d727c7f46ca4
LookasWasTaken~ <piacen.luca@gmail.com>
2023-05-08 15:29:12 +0200

    init

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- EXTERNAL PERSONAL CSS -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <title>To-Do-List</title>
+</head>
+
+<body>
+
+    <div id="app">
+
+
+    </div>
+    <!-- /#app -->
+
+
+    <!-- EXTERNAL BOOTSTRAP JS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+
+    <!-- EXTERNAL VUE JS CDN -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <!-- EXTERNAL PERSONAL JS -->
+    <script src="./assets/js/app.js"></script>
+
+</body>
+
+</html>
